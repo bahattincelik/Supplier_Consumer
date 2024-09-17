44ea67db8c06a7d8462691053f68c5962e759533
Bahattin Celik <celik@wave-solutions.de>
2024-09-17 16:17:09 +0200

    Thread

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,42 +8,58 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Supplier<String> tagesZeitenSupplier =() -> {
-            LocalTime now = LocalTime.now();
-            int hour = now.getHour();
+        Thread halloThread = new Thread(()-> {
+            try {
+                while (true) {
+                    Supplier<String> tagesZeitenSupplier =() -> {
+                        LocalTime now = LocalTime.now();
+                        int hour = now.getHour();
 
-            if (hour >= 6 && hour < 12) {
-                return "Morgen";
-            }else if (hour >= 12 && hour < 18) {
-                return "Nachmittag";
+                        if (hour >= 6 && hour < 12) {
+                            return "Morgen";
+                        }else if (hour >= 12 && hour < 18) {
+                            return "Nachmittag";
+                        }
+                        else if (hour >= 18 && hour < 24) {
+                            return "Abend";
+                        } else {
+                            return "Nacht";
+                        }
+                    };
+                    Consumer<String> halloConsumer = System.out::println;
+
+
+                    String tagesZeit = tagesZeitenSupplier.get();
+
+                    String halloMessage="";
+
+                    if(tagesZeit.equals("Morgen")){
+                        halloMessage = "Guten Morgen!";
+                    }
+                    else if(tagesZeit.equals("Nachmittag")){
+                        halloMessage = "Guten Tag!";
+                    }
+                    else if(tagesZeit.equals("Abend")){
+                        halloMessage = "Guten Abend!";
+                    }
+                    else {
+                        halloMessage = "Guten Nacht!";
+                    }
+
+                    halloConsumer.accept(halloMessage);
+
+                    Thread.sleep(5000);
+                }
+
+            } catch (InterruptedException e) {
+                System.out.println("Hallo-Thread wurde unterbrochen.");
             }
-            else if (hour >= 18 && hour < 24) {
-                return "Abend";
-            } else {
-                return "Nacht";
-            }
-        };
 
-        Consumer<String> halloConsumer = System.out::println;
+        });
+
+        halloThread.start();
 
 
-        String tagesZeit = tagesZeitenSupplier.get();
 
-        String halloMessage="";
-
-        if(tagesZeit.equals("Morgen")){
-            halloMessage = "Guten Morgen!";
-        }
-        else if(tagesZeit.equals("Nachmittag")){
-            halloMessage = "Guten Tag!";
-        }
-        else if(tagesZeit.equals("Abend")){
-            halloMessage = "Guten Abend!";
-        }
-        else {
-            halloMessage = "Guten Nacht!";
-        }
-
-        halloConsumer.accept(halloMessage);
     }
 }
